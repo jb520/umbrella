71e1456e5ea73af8cf494516a985a3fada77ac5a
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-02-21 19:07:26 +0000

    add umbrella message

--- umbrella.rb
+++ umbrella.rb
@@ -53,7 +53,32 @@ if minutely
   puts "Next hour: #{next_hour}"
 end
 
+
 hourly = pirate_parse.fetch("hourly")
 hourly_data = hourly.fetch("data")
 next_twelve_hours = hourly_data[1..12]
 
+precip_threshold = 0.10
+any_precip = false
+
+next_twelve_hours.each do |hour_hash|
+  precip_prob = hour_hash.fetch("precipProbability")
+
+  if precip_prob > precip_threshold
+    any_precip = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if any_precip == true
+  puts "You might want to take an umbrella!"
+else
+  puts "You probably won't need an umbrella."
+end
